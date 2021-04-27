dc81625183366289e3426f1b1ce930c7d0870052
Luan Mota Coleto <luancoleto@outlook.com>
2021-04-26 22:40:10 -0300

    Create post model

---
+++ src/modules/posts/infra/database/models/Post.ts
@@ -0,0 +1,19 @@
+class Post {
+  id: string;
+
+  title: string;
+
+  slug_title: string;
+
+  image: string;
+
+  description: string;
+
+  category_id: string;
+
+  created_at: Date;
+
+  updated_at: Date;
+}
+
+export default Post;
