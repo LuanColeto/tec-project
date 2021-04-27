3d90d18305092d771fc1b0b3c033e299f8f2fa42
Luan Mota Coleto <luancoleto@outlook.com>
2021-04-26 22:36:45 -0300

    Create user model

---
+++ src/modules/user/infra/database/models/User.ts
@@ -0,0 +1,17 @@
+class User {
+  id: string;
+
+  name: string;
+
+  avatar: string;
+
+  email: string;
+
+  password: string;
+
+  created_at: Date;
+
+  updated_at: Date;
+}
+
+export default User;
